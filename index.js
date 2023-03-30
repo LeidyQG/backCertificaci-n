7bf59be93db1b4b988570f62b4929d64b638370d
Leidy Quiñones <lalis.181014@gmail.com>
2023-03-30 17:39:17 -0500

    versión 0.0.1

---
+++ index.js
@@ -0,0 +1,63 @@
+const express=require('express');
+const app= express();
+app.use(express.json());
+
+app.use(function(req,res, next){
+    res.header("Access-Control-Allow-Origin","*");
+    res.header("Access-Control-Allow-Methods","POST"); 
+    res.header("Access-Control-Allow-Headers","Content-Type");
+    next();
+});
+
+app.post(
+    '/api/sumar',
+    (req, res)=>{
+        const {numero_1, numero_2}= req.body;
+        const resultado=parseFloat(numero_1)+parseFloat(numero_2);        
+        res.json(resultado);
+    }
+);
+
+app.post(
+    '/api/restar',
+    (req, res)=>{
+        const {numero_1, numero_2}= req.body;
+        const resultado=parseFloat(numero_1)-parseFloat(numero_2);        
+        res.json(resultado);
+    }
+);
+
+app.post(
+    '/api/multiplicar',
+    (req, res)=>{
+        const {numero_1, numero_2}= req.body;
+        const resultado=parseFloat(numero_1)*parseFloat(numero_2);        
+        res.json(resultado);
+    }
+);
+
+app.post(
+    '/api/dividir',
+    (req,res)=>{
+        let resultado;
+        
+        try{
+            const {numero_1, numero_2}= req.body;           
+            resultado=parseFloat(numero_1)/parseFloat(numero_2);   
+
+        }catch(error){
+            //Gestionar el error
+            resultado=error;
+        }
+        
+        res.json(resultado);
+    }
+)
+
+
+app.listen(
+    3000,
+    ()=>{
+      console.log("Servidor ejecutándose en el puerto 3000 con éxito");
+    }
+);
